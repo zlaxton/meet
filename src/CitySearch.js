ef93f1cfc0bd3b4d7fedf220793ee4906fdb5dd9
zlaxton <zlatan.kopic171@yahoo.com>
2021-10-25 17:51:35 +0200

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -8,22 +8,21 @@ class CitySearch extends Component {
   }
 
   handleInputChanged = (event) => {
-    const value = event.target.value;
-    const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
-    this.setState({
-      query: value,
-      suggestions,
-    });
-  };
-  handleItemClicked = (suggestion) => {
-    this.setState({
-      query: suggestion
-    });
-  }
-  
+  const value = event.target.value;
+  const suggestions = this.props.locations.filter((location) => {
+    return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+  });
+  this.setState({
+    query: value,
+    suggestions,
+  });
+};
 
+handleItemClicked = (suggestion) => {
+  this.setState({
+    query: suggestion
+  });
+}
 
   render() {
     return (
@@ -35,20 +34,10 @@ class CitySearch extends Component {
           onChange={this.handleInputChanged}
         />
         <ul className="suggestions">
-  {this.state.suggestions.map((suggestion) => (
-    <li
-      key={suggestion}
-      onClick={() => this.handleItemClicked(suggestion)}
-    >{suggestion}</li>
-  ))}
-  <li>
-    <b>See all cities</b>
-  </li>
-</ul>
+        </ul>
       </div>
     );
   }
 }
-
 
 export default CitySearch;

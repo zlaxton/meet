efbce2b9c395d983be397e67f88f879ed76a82c6
zlaxton <zlatan.kopic171@yahoo.com>
2022-01-17 19:48:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,91 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-import EventList from "./EventList";
-import CitySearch from "./CitySearch";
-import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
-import { WarningAlert } from "./Alert";
 import "./nprogress.css";
+import EventList from "./EventList";
+import NumberOfEvents from "./NumberOfEvents";
+import CitySearch from "./CitySearch";
+import { extractLocations, getAccessToken, getEvents, checkToken } from "./api";
+import Logo from "./Gatherr-logo.png";
+import { InfoAlert } from "./Alert";
 import WelcomeScreen from "./WelcomeScreen";
+import EventGenre from "./EventGenre";
 import {
+  ResponsiveContainer,
   ScatterChart,
   Scatter,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer,
 } from "recharts";
-import EventGenre from "./EventGenre";
 
-class App extends Component {
-  state = {
-    events: [],
-    locations: [],
-    currentLocation: "all",
-    numberOfEvents: 32,
-    warningText: "",
-    showWelcomeScreen: undefined,
-  };
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      events: [],
+      locations: [],
+      currentLocation: "all",
+      numberOfEvents: 32,
+      showWelcomeScreen: undefined,
+    };
+  }
 
-  async componentDidMount() {
+  componentDidMount = async () => {
     this.mounted = true;
     const accessToken = localStorage.getItem("access_token");
     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    /*this.setState({
-      showWelcomeScreen: !(
-        code ||
-        isTokenValid ||
-        window.location.hostname === 'localhost'
-      )
-    }); */
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({
-            events: events.slice(0, this.state.numberOfEvents),
-            locations: extractLocations(events),
-          });
-        }
+        this.setState({ events, locations: extractLocations(events) });
       });
     }
-    if (navigator.onLine) {
-      console.log("online");
-      this.setState({
-        warningText: "",
-      });
-    } else {
-      console.log("offline");
-      this.setState({
-        warningText: "You are currently using the app offline.",
-      });
-    }
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  updateEvents = (location, eventCount) => {
-    this.mounted = true;
-    getEvents().then((events) => {
-      const locationEvents =
-        location === "all" && eventCount === 0
-          ? events
-          : location !== "all" && eventCount === 0
-          ? events.filter((event) => event.location === location)
-          : events.slice(0, eventCount);
-      if (this.mounted) {
-        this.setState({
-          events: locationEvents,
-          numberOfEvents: eventCount,
-        });
-      }
-    });
+    // if (this.mounted) {
+    // 	getEvents().then(events => {
+    // 		this.setState({ events, locations: extractLocations(events) });
+    // 	});
+    // }
   };
+
+  componentWillUnmount = () => (this.mounted = false);
 
   getData = () => {
     const { locations, events } = this.state;
@@ -99,78 +64,74 @@ class App extends Component {
     return data;
   };
 
-  scrollTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      let locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents.slice(0, this.state.numberOfEvents),
+      });
     });
-  }
+  };
+
+  updateEventNumber = (eventNumber) => {
+    const { currentLocation } = this.state;
+    this.setState({ numberOfEvents: eventNumber });
+    this.updateEvents(currentLocation);
+  };
 
   render() {
-    if (this.state.showWelcomeScreen === undefined)
+    const { events, locations, numberOfEvents, showWelcomeScreen } = this.state;
+
+    if (showWelcomeScreen === undefined) {
       return <div className="App" />;
+    }
+
     return (
       <div className="App">
-        <h2>
-          Let's Meet-App{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            className="location-pin"
-            viewBox="0 0 16 16"
-          >
-            <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-            <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-          </svg>
-        </h2>
-        <WarningAlert text={this.state.warningText} />
+        <img src={Logo} alt="Gatherr-logo" className="logo" />
         <CitySearch
-          locations={this.state.locations}
+          locations={locations}
           updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
         />
         <NumberOfEvents
-          numberOfEvents={this.state.numberOfEvents}
-          updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
+          updateEventNumber={this.updateEventNumber}
         />
+        {!navigator.onLine && (
+          <InfoAlert text="Offline Mode: Data may not be up to date" />
+        )}
+        <h4>Events in each city</h4>
         <div className="data-vis-wrapper">
-          <EventGenre events={this.state.events} />
-          <ResponsiveContainer height={400}>
-            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+          <EventGenre events={events} />
+          <ResponsiveContainer width={400} height={400}>
+            <ScatterChart
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+              }}
+            >
               <CartesianGrid />
-              <XAxis
-                type="category"
-                dataKey="city"
-                name="city"
-                label={{
-                  value: "city",
-                  position: "insideBottomCenter",
-                  dy: 20,
-                }}
-              />
+              <XAxis type="category" dataKey="city" name="city" />
               <YAxis
                 allowDecimals={false}
                 type="number"
                 dataKey="number"
                 name="number of events"
-                label={{
-                  value: "number of events",
-                  angle: -90,
-                  position: "insideLeft",
-                }}
               />
               <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-              <Scatter data={this.getData()} fill="#de960b" />
+              <Scatter data={this.getData()} fill="#8884d8" />
             </ScatterChart>
           </ResponsiveContainer>
         </div>
-        <EventList className="eventlist" events={this.state.events} />
-        <button className="scrollup-btn" onClick={this.scrollTop}>
-          &#8593;
-        </button>
+        <EventList events={events} />
         <WelcomeScreen
-          showWelcomeScreen={this.state.showWelcomeScreen}
+          showWelcomeScreen={showWelcomeScreen}
           getAccessToken={() => {
             getAccessToken();
           }}
